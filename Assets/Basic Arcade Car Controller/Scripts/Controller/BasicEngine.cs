308cee8f1dbd048fb89819688b768b8bf560eaa2
brhnr99 <reyhani.burhan@gmail.com>
2026-07-07 20:43:26 +0300

    Engine torque calculations fixed.

--- Assets/Basic Arcade Car Controller/Scripts/Controller/BasicEngine.cs
+++ Assets/Basic Arcade Car Controller/Scripts/Controller/BasicEngine.cs
@@ -6,11 +6,10 @@ public class BasicEngine : MonoBehaviour
 
     BasicCarController basicCarController;
     BasicGearBox basicGearBox;
-    BasicDrivetrain basicDrivetrain;
 
     [Header("Engine Stats")]
     [SerializeField] AnimationCurve torqueCurve;
-    [SerializeField] float engineTorque = 1500f;
+    [SerializeField] float engineTorque = 300f;
     [SerializeField] float engineMaxRPM = 7000f;
     [SerializeField] float engineIdleRPM = 1000f;
     [SerializeField] float motorBrake = 1000f;
@@ -25,29 +24,30 @@ public class BasicEngine : MonoBehaviour
     [SerializeField] float maxJitter = 4;
 
     [Header("Engine Start")]
-    [SerializeField] float addTorqueValue = 20f;
+    [SerializeField] float addTorqueValue = 5f;
     [SerializeField] float addRPMValue = 40f;
-    [SerializeField] float startEngineTorque = 300f;
+    [SerializeField] float startEngineTorque = 100f;
     [SerializeField] float stallRPM = 900f;
 
     bool isRunning = false;
     bool ignite = false;
 
     public float currentRPM { get; private set; }
-
     public float currentEngineTorque { get; private set; }
+    public float currentNetEngTorque { get; private set; }
+    public float throttleVal { get; private set; }
 
     void Start()
     {
         carBody = GetComponent<Rigidbody>();
         basicCarController = GetComponent<BasicCarController>();
         basicGearBox = GetComponent<BasicGearBox>();
-        basicDrivetrain = GetComponent<BasicDrivetrain>();
     }
 
     void FixedUpdate()
     {
-        float throttleVal = basicCarController.carInputs.Drive.Throttle.ReadValue<float>(); // TODO: Look for more efficient methot.
+        throttleVal = basicCarController.carInputs.Drive.Throttle.ReadValue<float>(); // TODO: Look for more efficient methot.
+        //float throttleVal = basicGearBox.throttleValue;
 
         float startEngine = basicGearBox.inputMap.Drive.Ignition.ReadValue<float>();
         float killEngine = basicGearBox.inputMap.Drive.KillEngine.ReadValue<float>();
@@ -83,25 +83,26 @@ public class BasicEngine : MonoBehaviour
         bool disconnected = basicGearBox.currentGear == 0 || basicGearBox.clutchVal >= basicGearBox.GetClutchThreshold();
 
         float totalRatio = basicGearBox.TotalRatio();
-        // TODO: 4 is wheel count. Find more efficient way to count wheels!!
-        float reflectedInertia = (!disconnected && Mathf.Abs(totalRatio) > 0.0001f) ? (carBody.mass / 4) * basicCarController.driveWheels[0].radius
+        float wheelInertia = 20f; // This value will come from different wheel script. Temporarily here.
+        // Car's mass already calculated by the physic engine itself. So instead, adding wheel inertia will be more accurate I guess?
+        float reflectedInertia = (!disconnected && Mathf.Abs(totalRatio) > 0.0001f) ? wheelInertia * basicCarController.driveWheels[0].radius
         * basicCarController.driveWheels[0].radius / (totalRatio * totalRatio) : 0f;
 
+        currentNetEngTorque = currentEngineTorque - frictionTorque;
+
         float clutchTorque = disconnected ? 0f : basicGearBox.clutchTorque;
-        float netTorque = currentEngineTorque - frictionTorque - clutchTorque;
-        float angularAcceleration = netTorque / (engineInertia + reflectedInertia + basicGearBox.GearboxInertia());
+        float angularAcceleration = currentNetEngTorque - clutchTorque / (engineInertia + reflectedInertia + basicGearBox.GearboxInertia());
 
         float deltaRPM = angularAcceleration * (60 / (2f * Mathf.PI)) * Time.fixedDeltaTime;
-
-        Debug.Log("Reflected Inertia: " + reflectedInertia);
 
         float scaleJitter = 0.5f;
         float jitterValue = Random.Range(minJitter, maxJitter) * scaleJitter;
         currentRPM += deltaRPM + jitterValue;
         
-        if (basicGearBox.currentGear != 0 && basicGearBox.clutchVal < basicGearBox.GetClutchThreshold()) // && basicGearBox.GetJustShifted()
+        float clutchThreshold = 0.5f;
+        if (basicGearBox.currentGear != 0 && basicGearBox.clutchVal < clutchThreshold)
         {
-            float targetRPM = basicGearBox.ApplyGearRatio(basicCarController.avgWheelRPM) * 60f / (2f * Mathf.PI);
+            float targetRPM = basicGearBox.ApplyGearRatio(basicCarController.avgWheelRPM);
             float rpmSnapSpeed = 10f;
             currentRPM = Mathf.Lerp(currentRPM, targetRPM, Time.fixedDeltaTime * rpmSnapSpeed);
            
@@ -118,10 +119,10 @@ public class BasicEngine : MonoBehaviour
         {
             isRunning = false;
         }
-
+        
         if (currentRPM > engineMaxRPM)
         {
-            currentRPM = engineMaxRPM;
+            currentRPM -= 500f;
         }
     }
 
@@ -144,7 +145,8 @@ public class BasicEngine : MonoBehaviour
     // There are also other mechanical resistans that effect the the inertia. CalculateMotorBrake will handle that.
     float CalculateMotorBrake(float throttle)
     {
-        if (throttle < 0.1f)
+        float engineBrakeThreshold = 50f;
+        if (throttle < 0.1f && currentRPM > engineIdleRPM + engineBrakeThreshold)
         {
             float brakeTorque = motorBrake * (currentRPM / engineMaxRPM);
 
@@ -160,21 +162,21 @@ public class BasicEngine : MonoBehaviour
 
         if (ignite && !isRunning)
         {
-            currentEngineTorque += addTorqueValue;
+            currentNetEngTorque += addTorqueValue;
         }
-        else if (!ignite && !isRunning && Mathf.Abs(currentEngineTorque) >= 0)
+        else if (!ignite && !isRunning && Mathf.Abs(currentNetEngTorque) >= 0)
         {
-            currentEngineTorque -= addTorqueValue;
+            currentNetEngTorque -= addTorqueValue;
             currentRPM -= addRPMValue;
 
-            if (currentEngineTorque < 0)
-                currentEngineTorque = 0f;
+            if (currentNetEngTorque < 0)
+                currentNetEngTorque = 0f;
             
             if (currentRPM < 0)
                 currentRPM = 0;
         }
 
-        if (currentEngineTorque >= startEngineTorque)
+        if (currentNetEngTorque >= startEngineTorque)
         {
             currentRPM += addRPMValue;
 
@@ -205,5 +207,15 @@ public class BasicEngine : MonoBehaviour
     public float GetIdleEngineRPM()
     {
         return engineIdleRPM;
+    }
+
+    public float GetMaxEngineTorque()
+    {
+        return engineTorque;
+    }
+
+    public float GetEngineInertia()
+    {
+        return engineInertia;
     }
 }
